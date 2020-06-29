c3a1c342efe25d17ed8b4fd1f9306b57fde83f5f
Danielle Rubin <dlhrubin@gmail.com>
2020-06-29 18:40:36 -0400

    Set up GraphQL API, build schema

---
+++ src/graphql/queries.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      shows {
+        name
+        episodes {
+          name
+          season
+          number
+          watched
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        shows {
+          name
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
